535d5ac02f395c09cdd51e947880a93f70d2b657
Carlos Dourado <carlosismaelad@outlook.com>
2024-07-24 20:05:55 -0300

    Add User class and UserStatus enum

---
+++ Models/Enums/UserStatus.cs
@@ -0,0 +1,8 @@
+namespace TicketsApi.Models.Enums
+{
+    public enum UserStatus : int
+    {
+        Active = 0,
+        Disable = 1,
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,57 @@
+using System.ComponentModel.DataAnnotations;
+using TicketsApi.Models.Enums;
+
+namespace TicketsApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Role { get; set; }
+
+        [Required]
+        public UserStatus Status { get; set; } = UserStatus.Active;
+        public string CreatedAt { get; set; }
+
+        public string? UpdatedAt { get; set; }
+
+        public User() { }
+
+        public User(string username, string email, string password, string role)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                throw new ArgumentException("O campo nome não pode ser nulo ou vazio", nameof(username));
+            if (string.IsNullOrWhiteSpace(email))
+                throw new ArgumentException("O campo e-mail não pode ser nulo ou vazio", nameof(email));
+            if (string.IsNullOrWhiteSpace(password))
+                throw new ArgumentException("O campo e-mail não pode ser nulo ou vazio", nameof(password));
+            if (string.IsNullOrWhiteSpace(role))
+                throw new ArgumentException("O campo permissão não pode ser nulo ou vazio", nameof(role));
+
+            Username = username;
+            Email = email;
+            Password = password;
+            Role = role;
+            CreatedAt = GetBrazilTime();
+        }
+
+        public void SetUpdatedAt()
+        {
+            UpdatedAt = GetBrazilTime();
+        }
+
+        private string GetBrazilTime()
+        {
+            var BrazilTimeZone = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+            var FormatedBrazilTimeZone = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, BrazilTimeZone);
+            return FormatedBrazilTimeZone.ToString("dd/MM/yyyy HH:mm");
+        }
+    }
+}
