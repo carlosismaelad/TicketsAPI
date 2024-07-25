ae5e1f613ac0f38087399c2083cee29723585252
Carlos Dourado <carlosismaelad@outlook.com>
2024-07-24 23:10:40 -0300

    First migration and adding users seeding data

---
+++ Migrations/20240725020051_SeedInitial.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using BC = BCrypt.Net.BCrypt;
+
+#nullable disable
+
+namespace TicketsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Analyst = table.Column<string>(type: "text", nullable: false),
+                    Client = table.Column<string>(type: "text", nullable: false),
+                    NumberTicket = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+            // Inserção de dados iniciais na tabela 'Users'
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Username", "Email", "Password", "Role", "CreatedAt" },
+                values: new object[,]
+                {
+                    { "Batman", "batman@example.com", BC.HashPassword("AdminPassword123"), "Admin", DateTime.UtcNow },
+                    { "Robin", "robin@example.com", BC.HashPassword("UserPassword123"), "Default", DateTime.UtcNow }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
