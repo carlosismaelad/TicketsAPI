6f43994a49e0c72dca34aa687ff68f9166811f7b
carlosismaelad <carlosismaelad@outlook.com>
2024-07-25 11:42:35 -0300

    Correcting the type of columns to update the migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -19,29 +19,30 @@ namespace TicketsApi.Data
             {
                 entity.HasKey(t => t.Id);
                 entity.Property(t => t.Id)
-                    .ValueGeneratedOnAdd();
-                entity.Property(t => t.Title).IsRequired();
-                entity.Property(t => t.Analyst).IsRequired();
-                entity.Property(t => t.Client).IsRequired();
-                entity.Property(t => t.NumberTicket).IsRequired();
+                    .ValueGeneratedOnAdd()
+                    .UseIdentityColumn();
+                entity.Property(t => t.Id).HasColumnType("int");
+                entity.Property(t => t.Title).IsRequired().HasColumnType("nvarchar(max)");
+                entity.Property(t => t.Analyst).IsRequired().HasColumnType("nvarchar(40)");
+                entity.Property(t => t.Client).IsRequired().HasColumnType("nvarchar(40)");
+                entity.Property(t => t.NumberTicket).IsRequired().HasColumnType("nvarchar(max)");
                 entity.Property(t => t.Description).IsRequired().HasColumnType("text");
-                entity.Property(t => t.Status).HasConversion<int>().IsRequired();
+                entity.Property(t => t.Status).HasConversion<int>().IsRequired().HasColumnType("int");
                 entity.Property(t => t.CreatedAt).IsRequired().HasColumnType("datetime2");
                 entity.Property(t => t.UpdatedAt).IsRequired(false).HasColumnType("datetime2");
-
-
-
             });
             modelBuilder.Entity<User>(entity =>
             {
                 entity.HasKey(u => u.Id);
                 entity.Property(u => u.Id)
-                    .ValueGeneratedOnAdd();
-                entity.Property(u => u.Username).IsRequired();
-                entity.Property(u => u.Email).IsRequired();
-                entity.Property(u => u.Password).IsRequired();
-                entity.Property(u => u.Role).IsRequired();
-                entity.Property(u => u.Status).HasConversion<int>().IsRequired();
+                    .ValueGeneratedOnAdd()
+                    .UseIdentityColumn();
+                entity.Property(u => u.Id).HasColumnType("int");
+                entity.Property(u => u.Username).IsRequired().HasColumnType("nvarchar(40)");
+                entity.Property(u => u.Email).IsRequired().HasColumnType("nvarchar(100)");
+                entity.Property(u => u.Password).IsRequired().HasColumnType("nvarchar(max)");
+                entity.Property(u => u.Role).IsRequired().HasColumnType("nvarchar(10)");
+                entity.Property(u => u.Status).HasConversion<int>().IsRequired().HasColumnType("int");
                 entity.Property(u => u.CreatedAt).IsRequired().HasColumnType("datetime2");
                 entity.Property(u => u.UpdatedAt).IsRequired(false).HasColumnType("datetime2");
 
--- Migrations/20240725143056_SeedInitial.cs
+++ Migrations/20240725143056_SeedInitial.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 using TicketsApi.Models.Enums;
 using BC = BCrypt.Net.BCrypt;
 
@@ -19,8 +20,8 @@ namespace TicketsApi.Migrations
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Analyst = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Client = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Analyst = table.Column<string>(type: "nvarchar(40)", nullable: false),
+                    Client = table.Column<string>(type: "nvarchar(40)", nullable: false),
                     NumberTicket = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Description = table.Column<string>(type: "text", nullable: false),
                     Status = table.Column<int>(type: "int", nullable: false),
@@ -38,10 +39,10 @@ namespace TicketsApi.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(40)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", nullable: false),
                     Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(10)", nullable: false),
                     Status = table.Column<int>(type: "int", nullable: false),
                     CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
                     UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
@@ -55,8 +56,8 @@ namespace TicketsApi.Migrations
             columns: new[] { "Username", "Email", "Password", "Role", "Status", "CreatedAt" },
             values: new object[,]
             {
-                { "Batman", "batman@example.com", BC.HashPassword("AdminPassword123"), (int)UserStatus.Active, "Admin", DateTime.UtcNow },
-                { "Robin", "robin@example.com", BC.HashPassword("UserPassword123"), (int)UserStatus.Active, "Default", DateTime.UtcNow }
+                { "Batman", "batman@example.com", BC.HashPassword("AdminPassword123"), "Admin", (int)UserStatus.Active, DateTime.UtcNow },
+                { "Robin", "robin@example.com", BC.HashPassword("UserPassword123"), "Default", (int)UserStatus.Active, DateTime.UtcNow }
             });
         }
 
