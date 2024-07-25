af83ff7cabb5df36e51c309a57c4f26c1c33a821
Carlos Dourado <carlosismaelad@outlook.com>
2024-07-25 00:15:59 -0300

    First migration files

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -25,9 +25,9 @@ namespace TicketsApi.Data
                 entity.Property(t => t.Client).IsRequired();
                 entity.Property(t => t.NumberTicket).IsRequired();
                 entity.Property(t => t.Description).IsRequired().HasColumnType("text");
-                entity.Property(t => t.Status).IsRequired().HasConversion<int>();
-                entity.Property(t => t.CreatedAt).IsRequired().HasColumnType("timestamp");
-                entity.Property(t => t.UpdatedAt).IsRequired(false).HasColumnType("timestamp");
+                entity.Property(t => t.Status).HasConversion<int>().IsRequired();
+                entity.Property(t => t.CreatedAt).IsRequired().HasColumnType("datetime2");
+                entity.Property(t => t.UpdatedAt).IsRequired(false).HasColumnType("datetime2");
 
 
 
@@ -41,9 +41,9 @@ namespace TicketsApi.Data
                 entity.Property(u => u.Email).IsRequired();
                 entity.Property(u => u.Password).IsRequired();
                 entity.Property(u => u.Role).IsRequired();
-                entity.Property(u => u.Status).IsRequired().HasConversion<int>();
-                entity.Property(u => u.CreatedAt).IsRequired().HasColumnType("timestamp");
-                entity.Property(u => u.UpdatedAt).IsRequired(false).HasColumnType("timestamp");
+                entity.Property(u => u.Status).HasConversion<int>().IsRequired();
+                entity.Property(u => u.CreatedAt).IsRequired().HasColumnType("datetime2");
+                entity.Property(u => u.UpdatedAt).IsRequired(false).HasColumnType("datetime2");
 
 
             });
---
+++ Migrations/20240725024843_SeedInitial.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using TicketsApi.Models.Enums;
+using BC = BCrypt.Net.BCrypt;
+
+#nullable disable
+
+namespace TicketsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Analyst = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Client = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumberTicket = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+            migrationBuilder.InsertData(
+            table: "Users",
+            columns: new[] { "Username", "Email", "Password", "Role", "Status", "CreatedAt" },
+            values: new object[,]
+            {
+                { "Batman", "batman@example.com", BC.HashPassword("AdminPassword123"), (int)UserStatus.Active, "Admin", DateTime.UtcNow },
+                { "Robin", "robin@example.com", BC.HashPassword("UserPassword123"), (int)UserStatus.Active, "Default", DateTime.UtcNow }
+            });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
